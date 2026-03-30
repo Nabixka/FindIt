7859369c3d069abf8e9d06c6e312e54a8faf08c6
nabixka <nabixka05@gmail.com>
2026-03-30 21:32:19 +0700

    Fix Backend Report

--- backend/multer.js
+++ backend/multer.js
@@ -2,12 +2,16 @@ const multer = require("multer")
 const path = require("path")
 
 const storage = multer.diskStorage({
+
     destination: (req, file, cb) => {
+        console.log("Base URL: ", req.baseUrl)
+        console.log("FIeldname : ", file.fieldname)
+
         let folder = "uploads"
         if(file.fieldname === "image" && req.baseUrl.includes("item")){
             folder = "uploads/items"
         }
-        if(file.fieldname === "image" && req.baseUrl.includes("report")){
+        if(file.fieldname === "proof" && req.baseUrl.includes("user")){
             folder = "uploads/report"
         }
 
